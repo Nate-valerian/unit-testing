b27814ee2ee4f8f94e4d3bdffa291fdc383a7040
Nate Valerian <n.nate1544@gmail.com>
2025-12-16 16:40:57 +0300

    testing files have been created

---
+++ __tests__/imageUpload.test.tsx
@@ -0,0 +1,34 @@
+import {render, screen, fireEvent, } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { ImageUpload } from '../components/input-fields/imageUpload';
+
+describe('ImageUpload', () => {
+  const mockHandleChange = jest.fn();
+
+
+  it('calss handleChange with correct when a file is selected', () => {
+    render(<ImageUpload handleChange={mockHandleChange}/>);
+
+    const file = new File(['dummy content'], 'profile.png', { type: 'image/png' });
+
+    const fileInput = screen.getByTestId('file-upload');
+    fireEvent.change(fileInput, { target: { files: [file] } });
+
+    expect(mockHandleChange).toHaveBeenCalledTimes(1);
+    expect(mockHandleChange).toHaveBeenCalledWith(
+      expect.objectContaining({
+        target: expect.objectContaining({
+          files: expect.arrayContaining([file]),
+
+
+        }),
+      }),
+
+    );
+
+
+
+  })
+
+
+});
---
+++ __tests__/inputField.test.tsx
@@ -0,0 +1,54 @@
+import '@testing-library/jest-dom';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { InputField } from '../components/input-fields/inputField';
+
+    describe('InputField', () => {
+      const label = 'First Name';
+      const name = 'firstName';
+      const placeholder = 'nate';
+
+
+      it('renders correctly with label and placeholder', () => {
+        render(<InputField label={label} name={name} value="" onChange={() => {}} placeholder={placeholder} required />);
+
+        const input = screen.getByPlaceholderText(placeholder);
+        expect(input).toBeInTheDocument();
+
+        const labelElement = screen.getByText(label);
+        expect(labelElement).toBeInTheDocument();
+
+      });
+
+
+      it('calls onChange when typing', () => {
+        const handleChange = jest.fn();
+        render(<InputField label={label} name={name} value="" onChange={handleChange} placeholder={placeholder} required />);
+        const input = screen.getByRole('textbox');
+
+        fireEvent.change(input, { target: { value: 'John' } });
+        expect(handleChange).toHaveBeenCalledTimes(1);
+
+      });
+
+
+      it('shows error if required and blurred with empty value', () => {
+        render(<InputField label={label} name={name} value="" onChange={() => {}} placeholder={placeholder} required />);
+        const input = screen.getByRole('textbox');
+        fireEvent.blur(input);
+        const errorMessage = screen.getByText(`${label} is required`);
+        expect(errorMessage).toBeInTheDocument();
+
+      });
+
+      it('does not show error if required and if value is present', () => {
+        render(<InputField label={label} name={name} value="nate" onChange={() => {}} placeholder={placeholder} required />);
+        const input = screen.getByRole('textbox');
+        fireEvent.blur(input);
+        const errorMessage = screen.queryByText(`${label} is required`);
+        expect(errorMessage).not.toBeInTheDocument();
+
+      });
+
+    })
+
+
